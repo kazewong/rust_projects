dbfa65234a0469a610f2f0d06762fbedad260bcb
kazewong <kazewong.physics@gmail.com>
2024-01-01 15:35:26 -0500

    Seems doing something

--- wgpu_wasm/src/lib.rs
+++ wgpu_wasm/src/lib.rs
@@ -1,15 +1,28 @@
-use wasm_bindgen::prelude::*;
 use console_log;
-use log::debug;
+use log::{debug, info};
+use wasm_bindgen::prelude::*;
+use winit::{event_loop::EventLoop, window::WindowBuilder};
 
 mod runtime;
 
-#[wasm_bindgen]
+#[wasm_bindgen(start)]
 pub async fn run() {
 
     console_log::init_with_level(log::Level::Debug).expect("error initializing log");
 
+    // Getting the canvas element
     let canvas = runtime::get_canvas();
     let (width, height) = (canvas.client_width(), canvas.client_height());
     debug!("Acquired canvas with size {}x{}", width, height);
+
+    // Creating the window
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new()
+        .build(&event_loop)
+        .map(|window| {
+            let _ = window.request_inner_size(winit::dpi::LogicalSize::new(width, height));
+            window
+            })
+        .expect("failed to create window");    
+    debug!("Created window");
 }

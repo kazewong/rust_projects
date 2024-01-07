4c09048c881bab423811a0722c7b2568709391c8
Kaze Wong <kazewong.physics@gmail.com>
2024-01-06 23:30:56 -0500

    Add initialization and skeleton methods to WGPU context

--- nbody_wasm/src/wgpu_context.rs
+++ nbody_wasm/src/wgpu_context.rs
@@ -1,4 +1,5 @@
 use wgpu;
+
 struct Context{
     surface: wgpu::Surface,
     device: wgpu::Device,
@@ -11,6 +12,28 @@ impl Context{
     fn init(
 
     ){
+        // Initialize compute shader
+
+        // Initialize compute bind group
+
+        // Initialize compute pipeline
+
+        // Initialize render shader
+
+        // Initialize render bind group
+
+        // Initialize render pipeline
+    }
+
+    fn update(){
         
+    }
+
+    fn resize(){
+
+    }
+
+    fn render(){
+
     }
 }

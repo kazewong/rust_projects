cfed05babe868066b73c60e2db8bf84768fa6a49
Kaze Wong <kazewong.physics@gmail.com>
2024-01-06 22:44:45 -0500

    Add wgpu_context.rs and multithread/main.rs

---
+++ nbody_wasm/src/wgpu_context.rs
@@ -0,0 +1,16 @@
+use wgpu;
+struct Context{
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+
+    render_pipeline: wgpu::RenderPipeline,
+    compute_pipeline: wgpu::ComputePipeline,
+}
+
+impl Context{
+    fn init(
+
+    ){
+        
+    }
+}
